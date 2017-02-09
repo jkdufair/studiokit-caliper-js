b1d2cb334bf6e5c8cc5aa3bda1c4f83b59edb62f
Will Grauvogel <builtbywill@gmail.com>
2017-02-08 22:36:34 -0500

    general cleanup, add browserify grunt to build a standalone file for use with non-node apps

---
+++ gruntfile.js
@@ -0,0 +1,69 @@
+module.exports = function(grunt) {
+
+	// Project configuration.
+	grunt.initConfig({
+		pkg: grunt.file.readJSON('package.json'),
+		browserify: {
+			dist: {
+				files: {
+					'dist/studiokit-caliper.js': ['lib/index.js']
+				},
+				options: {
+					browserifyOptions: {
+						standalone: 'StudioKit'
+					}
+				}
+			}
+		},
+		'http-server': {
+			'dev': {
+				// the server root directory
+				root: '.',
+				port: 8888,
+				host: "127.0.0.1",
+				cache: 0,
+				showDir: true,
+				autoIndex: true,
+				// server default file extension
+				ext: "html",
+				// run in parallel with other tasks
+				runInBackground: false
+			}
+		},
+		tape: {
+			options: {
+				pretty: true,
+				output: 'console'
+			},
+			files: ['test/*.js']
+		},
+		jsdoc: {
+			dist: {
+				src: [
+					'src/**/*.js', 'README-external.md'
+				],
+				options: {
+					destination: 'doc'
+				}
+			}
+		}
+	});
+
+	// Load the plugin and tasks that provides unit testing via tap/tape
+	// grunt.loadNpmTasks('grunt-tape');
+	// grunt.registerTask('test', ['tape']);
+	// grunt.registerTask('ci', ['tape:ci']);
+
+	// Load the plugin that provides the "browserify" task.
+	grunt.loadNpmTasks('grunt-browserify');
+
+	// Load plugin that provides http-server task
+	// grunt.loadNpmTasks('grunt-http-server');
+
+	// Load jsDoc3 plugin
+	// grunt.loadNpmTasks('grunt-jsdoc');
+
+	// Default task(s).
+	// grunt.registerTask('default', ['test', 'browserify', 'jsdoc']);
+	grunt.registerTask('default', ['browserify']);
+};
